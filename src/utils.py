142da7918dcc2b05eb107edef6bfdc07e288280a
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-05-24 19:05:08 -0300

    feat(model): implementa pipeline de treino com BERT HuggingFace

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import os
+
+
+def ensure_dirs():
+    folders = [
+        "data/processed",
+        "outputs",
+        "metrics",
+        "predictions",
+        "models/bert"
+    ]
+
+    for folder in folders:
+        os.makedirs(
+            folder,
+            exist_ok=True
+        )
